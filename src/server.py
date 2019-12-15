dd88da875a78770ce9adea5749e29a007f88aa6e
pol_gil <pol.gil.hernandez@es.ey.com>
2019-12-15 11:30:52 +0100

    changes to class

--- src/server.py
+++ src/server.py
@@ -6,23 +6,23 @@ cursor = db.cursor()
 
 #cursor.execute("CREATE TABLE `users` (`id` INT NOT NULL, `name` TEXT NOT NULL, `age` INT NOT NULL, `sex` TEXT NOT NULL, latitude FLOAT, longitude FLOAT, PRIMARY KEY(`id`));")
 
-def create(self, id, name, age, sex, latitude, longitude): 
+def create(id, name, age, sex, latitude, longitude): 
     sqlinsert = ("INSERT INTO `users` (id, name, age, sex, latitude, longitude) VALUES (%s, %s, %s, %s, %s, %s);")
     cursor.execute(sqlinsert, (id, name, age, sex, latitude, longitude))
 
-def getuser(self, id):
+def getuser(id):
     sql = ("SELECT * FROM `users` WHERE id = %s")
     cursor.execute(sql, id)
     data = cursor.fetchone()
     print("Mi contenido es el siguiente: ", data)
 
-def existuser(self, id):
+def existuser(id):
     sql = ("SELECT COUNT(*) FROM `users` WHERE id = %s")
     cursor.execute(sql, id)
     data = cursor.fetchone()
     return data == 0
 
-def updateuser(self, id, name, age, sex, latitude, longitude):
+def updateuser(id, name, age, sex, latitude, longitude):
     sqlupdate = ("UPDATE `users` SET name = %s, age = %s, sex = %s, latitude = %s, longitude = %s WHERE id = %s")
     cursor.execute(sqlupdate, (name, age, sex, latitude, longitude, id))
 
