ba44a210dbaabeeacc18b0abd0c7c89b7a2c7f5d
pol_gil <pol.gil.hernandez@es.ey.com>
2019-12-15 11:03:47 +0100

    changes on server

--- src/main.py
+++ src/main.py
@@ -201,7 +201,7 @@ def saveUser(bot, update, user_data):
         answer = update.message.text
         if(answer.startswith("1.")):
             #crear Usuario con los datos de user_data, si existe ya updatearlo (la logica de si insert o update la pondria en el archivo server)
-            server.create(userID, user_data.get("name"), user_data.get("age"), user_data.get("sex"), user_data.get("latitude"), user_data.get("longitude"))
+            server.create(userId, user_data.get("name"), user_data.get("age"), user_data.get("sex"), user_data.get("latitude"), user_data.get("longitude"))
             bot.send_message(chat_id=update.message.chat_id, text=translate("userSaved"))
         return SAVE_USER
 
@@ -272,8 +272,9 @@ def answers(bot, update, user_data, _early_response=[None]):
         bot.send_message(chat_id=update.message.chat_id, text="💣{}".format(str(e)))
         '''
 def main():
-    #949321682:AAECbZCBtEFHbLDYELQ2OHuFNfpnQcmp5J8 POL
-    token = "942283486:AAGVxx31KxtIzMGgZkBZBRWoC5POwcrtRUw" #open('../token.txt').read().strip()
+    #949321682:AAECbZCBtEFHbLDYELQ2OHuFNfpnQcmp5J8 TEST1
+    #942283486:AAGVxx31KxtIzMGgZkBZBRWoC5POwcrtRUw TEST2
+    token = "949321682:AAECbZCBtEFHbLDYELQ2OHuFNfpnQcmp5J8" #open('../token.txt').read().strip()
     print("el token es" + token)
     updater = Updater(token=token)
     dispatcher = updater.dispatcher
